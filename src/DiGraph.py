03dace05a46f8f3f5d3cd1a1eb69405e8e0de623
Mark Vertenberg <markvertenberg2001@gmail.com>
2021-12-23 10:10:00 +0200

    DiGraph.py is changed

--- src/DiGraph.py
+++ src/DiGraph.py
@@ -1,22 +1,28 @@
 from GraphInterface import GraphInterface
+import math
 
 
 class DiGraph(GraphInterface):
 
+    def __init__(self):
+        self.nodes = {}
+        self.edges = {}
+        self.mc = 0
+
     def v_size(self):
-        pass
+        return len(self.nodes)
 
     def e_size(self):
-        pass
+        return len(self.edges)
 
     def get_all_v(self):
-        pass
+        return self.nodes
 
     def all_in_edges_of_node(self, id1: int):
-        pass
+        return self.nodes[id1].inEdges
 
     def all_out_edges_of_node(self, id1: int):
-        pass
+        return self.nodes[id1].outEdges
 
     def get_mc(self):
         pass
@@ -32,3 +38,27 @@ class DiGraph(GraphInterface):
 
     def remove_edge(self, node_id1: int, node_id2: int):
         pass
+
+
+class Node:
+    def __init__(self, key, x, y):
+        self.key = key
+        self.x = x
+        self.y = y
+        self.outEdges = {}
+        self.inEdges = {}
+
+    def getKey(self):
+        return self.key
+
+    def x(self):
+        return self.x
+
+    def y(self):
+        return self.y
+
+    def distance(self, other):
+        x = math.pow((other.getx() - self.x()), 2)
+        y = math.pow((other.gety() - self.y()), 2)
+        return math.sqrt(x + y)
+
