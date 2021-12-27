7ec45a73ee27345ef1eadaed554805f618f35e9a
Mark Vertenberg <markvertenberg2001@gmail.com>
2021-12-27 23:59:40 +0200

    edges adjusted to right place

--- src/graphics/EdgePainter.py
+++ src/graphics/EdgePainter.py
@@ -23,12 +23,13 @@ class EdgePainter:
         if self.dest.new_x and self.dest.new_y:
             dx = self.dest.new_x - self.src.new_x
             dy = self.dest.new_y - self.src.new_y
-            a = math.tan(dy / dx)
+            a = math.atan(abs(dy / dx))
+            print(a)
             s_h = math.sin(a) * self.src.radius
             s_w = math.cos(a) * self.src.radius
             d_h = math.sin(a) * self.dest.radius
             d_w = math.cos(a) * self.dest.radius
-            if dx > 0 and dy > 0:
+            if dx >= 0 and dy >= 0:
                 return self.src.new_x + s_w, self.src.new_y + s_h, self.dest.new_x - d_w, self.dest.new_y - d_h
             if dx > 0 and dy < 0:
                 return self.src.new_x + s_w, self.src.new_y - s_h, self.dest.new_x - d_w, self.dest.new_y + d_h
@@ -42,4 +43,5 @@ class EdgePainter:
         self.start_x, self.start_y, self.stop_x, self.stop_y = self.start_pos()
         if self.start_x:
             pygame.draw.line(screen, self.color, (self.start_x, self.start_y), (self.stop_x, self.stop_y), outline)
+
 
